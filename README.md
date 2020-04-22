7efdee90b735c8ce2eec1fdee01b72cb59d8801c
janani.sampathkumar <janani.sampathkumar@mirriad.com>
2020-04-22 14:04:55 +0100

    Committing - Task 1 : Checking identifier

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # kotlin-tasks
+
+<b>Task 1: Checking identifier</b> <br>
+Implement the function that checks whether a string is a valid identifier. A valid identifier is a non-empty string that starts with a letter or underscore and consists of only letters, digits and underscores.
---
+++ src/main/kotlin/Task_1.kt
@@ -0,0 +1,28 @@
+/**
+ * Implement the function that checks whether a string is a valid identifier.
+ * A valid identifier is a non-empty string that starts with a letter or underscore
+ * and consists of only letters, digits and underscores.
+ */
+fun isValidIdentifier(s: String): Boolean {
+    if (s.isEmpty())
+        return false
+
+    if (!(s[0].isLetter() || s[0] == '_'))
+        return false
+
+    for (c in s.substring(1 until s.length)) {
+        if (!(c.isLetter() || c.isDigit() || c == '_'))
+            return false
+    }
+
+    return true
+}
+
+fun main(args: Array<String>) {
+    println(isValidIdentifier("name"))   // true
+    println(isValidIdentifier("_name"))  // true
+    println(isValidIdentifier("_12"))    // true
+    println(isValidIdentifier(""))       // false
+    println(isValidIdentifier("012"))    // false
+    println(isValidIdentifier("no$"))    // false
+}
